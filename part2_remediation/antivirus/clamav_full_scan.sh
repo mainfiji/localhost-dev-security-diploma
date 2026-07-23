49771731584765286dc143fbd054eaca2741efcc
Sasha <reedumq@gmail.com>
2026-07-24 00:06:33 +0300

    Update clamav_full_scan.sh

--- part2_remediation/antivirus/clamav_full_scan.sh
+++ part2_remediation/antivirus/clamav_full_scan.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+LOGFILE="/var/log/clamav/clamav_scan_$(date +%Y%m%d).log"
+echo "Starting full system scan at $(date)" >> $LOGFILE
+clamscan -r / --exclude-dir=/sys --exclude-dir=/proc --exclude-dir=/dev --exclude-dir=/run --exclude-dir=/mnt --exclude-dir=/media --exclude-dir=/var/log/clamav -l $LOGFILE
+echo "Scan finished at $(date)" >> $LOGFILE
